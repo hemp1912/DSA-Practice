de9b2d68512c5a613d10d3cda44e16c3bff7012e
hemp1912 <hemp1912@gmail.com>
2025-08-06 14:49:50 +0530

    add p4

--- p4.java
+++ p4.java
@@ -1,5 +1,15 @@
+import java.util.Stack;
+
 public class p4 {
     public static void main(String[] args) {
-        
+        Stack<String> fruit = new Stack<>();
+        fruit.push("Apple");
+        fruit.push("Banana");
+        fruit.push("Cherry");
+        fruit.push("Date");
+        fruit.push("Elderberry");
+        while (!fruit.isEmpty()) {
+            System.out.println("Fruit: " + fruit.pop());
+        }
     }
 }
